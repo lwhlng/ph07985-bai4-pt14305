d2b8fabb514cf957423631bd1ca273ef18046171
lwhlng <hoanglogg@gmail.com>
2020-04-18 23:50:57 +0700

    4 files modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+// import { HomeComponent } from './home/home.component';
+import { IndexAdminComponent } from './admin/index-admin/index-admin.component';
 import { ProductManagerComponent } from './product-manager/product-manager.component';
 import { AboutComponent } from './about/about.component';
-
+import { IndexclientComponent } from './indexclient/indexclient.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'home', pathMatch: 'full'},
-  {path: 'home', component: HomeComponent},
-  {path: 'manager', component: ProductManagerComponent},
-  {path: 'about', component: AboutComponent}
+  {path: '', redirectTo: 'client', pathMatch: 'full'},
+  {path: 'client', component: IndexclientComponent,
+  children:[
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path: 'home', component: HomeComponent},
+    {path: 'manager', component: ProductManagerComponent},
+    {path: 'about', component: AboutComponent}
+  ]
+  },
+  {path: 'admin', component: IndexAdminComponent,},
 ];
 
 @NgModule({
